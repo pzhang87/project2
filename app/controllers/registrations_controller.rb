1d11be7c93e03629196befba222c26c3075ad26f
Peter Zhang <pzhang87@gmail.com>
2015-11-18 20:08:16 -0500

    implement add/remove teams to tournaments via registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,9 +5,9 @@ class RegistrationsController < ApplicationController
     @registrations = @tournament.registrations
   end
 
-  def show
-    @registration = Registration.find(params[:id])
-  end
+  # def show
+  #   @registration = Registration.find(params[:id])
+  # end
 
   def new
     @registration = Registration.new
@@ -16,28 +16,30 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-
+    @tournament = Tournament.find(params[:tournament_id])
+    @tournament.registrations.create(registration_params)
+    redirect_to tournament_registrations_path(@tournament)
   end
 
-  def edit
-    @team = Team.find(params[:id])
-  end
+  # def edit
+  #   @team = Team.find(params[:id])
+  # end
 
-  def update
-    @team = Team.find(params[:id])
-    @team.update(team_params)
-    redirect_to team_path(@team)
-  end
+  # def update
+  #   @team = Team.find(params[:id])
+  #   @team.update(team_params)
+  #   redirect_to team_path(@team)
+  # end
 
   def destroy
-    @team = Team.find(params[:id])
-    @team.destroy
-    redirect_to teams_path
+    @registration = Registration.find(params[:id])
+    @registration.destroy
+    redirect_to tournament_registrations_path
   end
 
   private
-  def team_params
-    params.require(:team).permit(:team_name,
-    :team_description, :team_logo)
+  def registration_params
+    params.require(:registration).permit(:team_id,
+    :tournament_id)
   end
 end
